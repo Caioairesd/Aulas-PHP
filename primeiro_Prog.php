697efda85e0dae6fa51da6fd8cd88e3d3f1df120
Caio Vinicius <caioviniaires@gmail.com>
2025-07-08 22:07:13 -0300

    update

--- primeiro_Prog.php
+++ primeiro_Prog.php
@@ -1,23 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 
 
 
     <?php
 
-        phpinfo()
-        
+    print "Teste<br>";
+    print "Olá, mundo\n<br>";
+    print "Escpe 'charn' são os MESMOS como em C\n<br>";
+    print "Você pode ter quebras de linhas em uma string<br>";
+    print 'Uma String pode usar "aspas-duplas". Isso é muito legal!<br>';
+    print 'Ainda pode-se usar aspas simples dessa forma it\'s cool<br>';
 
-    ?>
+    echo "Texto<br>";
+    echo "Olá mundo<br>";
+    echo "Isso abrange
+       várias linhas também<br>";
+    echo "Isso abrange \nmultiplas linhas. A nova linha será<br>";
+    echo "Caracteres Escaping são feitos .<br>";
+
+    //Comentário uma linha
+    #Também para uma linha
+
+    /*Mais
+    de uma 
+    linha*/
+    
+    ?>  
 
 
 
 </body>
+
 </html>
