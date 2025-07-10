1367cc5930f2ac4fdeaa887dd0483e84993683fd
Caio Vinicius <caioviniaires@gmail.com>
2025-07-09 22:08:39 -0300

    Mais atividades

---
+++ Exemplo3.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+        $num = 537; //Número intero 
+        echo $num, "Número inteiro <br/><br/>";
+
+        $num = -13; //Número negativo
+        echo $num,"Número negativo <br/><br/>";
+
+        $num = 0123; //Número octal (Equivalente a 83 decimal)
+        echo $num,"Número octal <br/><br/>";
+
+        $num = 0x1A; //Número hexadecimal 
+        echo $num,"Número hexadecimal <br/><br/>";
+
+        $num = 1.234; //Ponto flutuante)
+        echo $num,"Ponto flutuante <br/><br/>";
+
+        $num = 4e23; //Notação cientifica
+        echo $num,"Notação cientifica <br/><br/>";
+
+      
+
+
+
+
+
+    ?>
+
+</body>
+
+</html>
