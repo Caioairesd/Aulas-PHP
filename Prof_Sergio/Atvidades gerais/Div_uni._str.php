360f4f8c480055456999ce5872402319c5d53eea
Caio Vinicius <caioviniaires@gmail.com>
2025-07-17 22:13:42 -0300

    Adição de mias atividades
    - Inclusão de Section;

---
+++ Prof_Sergio/Atvidades gerais/Div_uni._str.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $turma = "Técnico de Desenvolvimento de sistemas";
+        echo"Turma: ".$turma."<br/>";
+        $turma1 = explode(" ",$turma);
+        echo"Turma1: "; print_r($turma1); echo"<br/>";
+        $turma2 = implode("...",$turma1);
+        echo "Turma2: $turma2 <br/>";
+        
+    
+    
+    
+    
+    
+    ?>    
+
+
+</body>
+</html>
