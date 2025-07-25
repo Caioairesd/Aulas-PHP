9c87fb6315a7313700b4a2abcb5035f5c7de4c0d
Caio Vinicius <caioviniaires@gmail.com>
2025-07-24 22:13:41 -0300

    Fix
    Fix: Corrigindo erros de String e int.

--- Prof_Sergio/Formulario/banco.php
+++ Prof_Sergio/Formulario/banco.php
@@ -1,42 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $bdServidor = '127.0.0.1';
-        $bdUsuario = 'root';
-        $bdSenha = '';
-        $bdBanco = 'caio_silva';
-        $conexao = mysqli_connect( $bdServidor,$bdUsuario,$bdSenha,$bdBanco );
-        if (mysqli_connect_errno()){
-            echo"Problemas para conectar no banco. Verifique os dados!";
-            die();
+    $bdServidor = '127.0.0.1';
+    $bdUsuario = 'root';
+    $bdSenha = '';
+    $bdBanco = 'caio_silva';
+    $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+    if (mysqli_connect_errno()) {
+        echo "Problemas para conectar no banco. Verifique os dados!";
+        die();
+
+    }
+
+
+
+    function buscar_tarefas($conexao)
+    {
+
+        $sqlBusca = 'SELECT * FROM tarefas';
+        $resultado = mysqli_query($conexao, $sqlBusca);
+        $tarefas = array();
+        while ($tarefa = mysqli_fetch_assoc($resultado)) {
+            $tarefas = $tarefa;
 
         }
 
 
-        
-        function buscar_tarefas($conexao) {
+        return $tarefas;
+    }
 
-            $sqlBusca = 'SELECT * FROM tarefas';
-            $resultado = mysqli_query($conexao,$sqlBusca);
-            $tarefas = array();
-            while($tarefa = mysqli_fetch_assoc($resultado)){
-                $tarefas = $tarefa;
+    function gravar_tarefa($conexao, $tarefa)
+    {
 
-            }
-
-
-        return $tarefas;    
-        }
-
-        function gravar_tarefa($conexao,$tarefa){
-
-            $sqlGravar =" INSERT INTO tarefas
+        $sqlGravar = " INSERT INTO tarefas
             (nome,descricao,prioridade,prazo,concluida,)
             values
             (
@@ -50,14 +54,15 @@
             
             )
             ";
-            mysqli_query($conexao,$sqlGravar);
+        mysqli_query($conexao, $sqlGravar);
 
 
 
 
-        }
-    
-    
+    }
+
+
     ?>
 </body>
+
 </html>
